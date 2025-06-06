e92320ebeac29e6571ca3ae171ff19c21a1dbc33
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-06-06 11:48:33 -0700

    ci archetype: add build prefixes to versioned formula

--- archetype/contents/Formula/{{ binary }}@{{ versionl}}.rb
+++ archetype/contents/Formula/{{ binary }}@{{ versionl}}.rb
@@ -1,27 +1,27 @@
 class {{ binary | pascal_case }}AT{{ version | replace(".", "_") }} < Formula
   desc "{{ description }}"
   homepage "{{ homepage }}"
-  version "{{ version }}"{% if linux-x86_64_archive or linux-arm64_archive %}
+  version "{{ version }}"{% if builds.linux-x86_64_archive or builds.linux-arm64_archive %}
 
   on_linux do
-    {% if linux-x86_64_archive %}if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "{{ linux-x86_64_archive }}"
-      sha256 "{{ linux-x86_64_sha256 }}"
-    end{% endif %}{% if linux-arm64_archive %}
+    {% if builds.linux-x86_64_archive %}if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+      url "{{ builds.linux-x86_64_archive }}"
+      sha256 "{{ builds.linux-x86_64_sha256 }}"
+    end{% endif %}{% if builds.linux-arm64_archive %}
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "{{ linux-arm64_archive }}"
-      sha256 "{{ linux-arm64_sha256 }}"
+      url "{{ builds.linux-arm64_archive }}"
+      sha256 "{{ builds.linux-arm64_sha256 }}"
     end{% endif %}
-  end{% endif %}{% if macos-x86_64_archive or macos-arm64_archive %}
+  end{% endif %}{% if builds.macos-x86_64_archive or builds.macos-arm64_archive %}
 
-  on_macos do{% if macos-x86_64_archive %}
+  on_macos do{% if builds.macos-x86_64_archive %}
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "{{ macos-x86_64_archive }}"
-      sha256 "{{ macos-x86_64_sha256 }}"
-    end{% endif %}{% if macos-arm64_archive %}
+      url "{{ builds.macos-x86_64_archive }}"
+      sha256 "{{ builds.macos-x86_64_sha256 }}"
+    end{% endif %}{% if builds.macos-arm64_archive %}
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "{{ macos-arm64_archive }}"
-      sha256 "{{ macos-arm64_sha256 }}"
+      url "{{ builds.macos-arm64_archive }}"
+      sha256 "{{ builds.macos-arm64_sha256 }}"
     end{% endif %}
   end{% endif %}
 
